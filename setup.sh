82975a851ec5f1dcd5bca4cca053f28bd7f7a54b
Simon Persson <simon@flaskpost.org>
2014-09-14 00:03:41 +0200

    Download MonteCarlo font automatically.

--- setup.sh
+++ setup.sh
@@ -29,3 +29,13 @@ cp vim/vimrc $DEST/.vimrc
 cp screen/screenrc $DEST/.screenrc
 
 cp zsh/zshrc $DEST/.zshrc
+
+if [ ! -d "$DEST/.fonts" ]; then
+	mkdir "$DEST/.fonts"
+fi
+
+wget http://www.bok.net/MonteCarlo/downloads/MonteCarlo-PCF.tgz -O $DEST/.fonts/MonteCarlo-PCF.tgz
+tar zxfv $DEST/.fonts/MonteCarlo-PCF.tgz -C $DEST/.fonts/
+mv $DEST/.fonts/MonteCarlo-PCF/* $DEST/.fonts/
+rm -rf $DEST/.fonts/MonteCarlo-PCF $DEST/.fonts/MonteCarlo-PCF.tgz
+mkfontdir $DEST/.fonts
