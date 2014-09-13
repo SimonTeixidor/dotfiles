86f64fa47aac559345808083ee325b4105dba5c7
Simon Persson <simon@flaskpost.org>
2014-09-13 23:00:12 +0200

    Some updates

--- setup.sh
+++ setup.sh
@@ -8,6 +8,18 @@ if [ ! -d "$DEST/.ncmpcpp" ]; then
 	mkdir "$DEST/.ncmpcpp"
 fi
 
+cd dwm-6.0
+make
+if [ ! -d "$DEST/bin" ]; then
+	mkdir "$DEST/bin
+fi
+cp dwm $DEST/bin
+cd ..
+
+cp misc/xinitrc $DEST/.xinitrc
+
+cp scripts/startdwm $DEST/bin/
+
 cp ncmpcpp/config $DEST/.ncmpcpp/
 
 cp urxvt/Xdefaults $DEST/.Xdefaults
