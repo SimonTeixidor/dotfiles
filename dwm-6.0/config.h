e32da3371766b22be06b4653b9c5fccf90baef6c
Simon Persson <simon@flaskpost.org>
2016-02-11 17:00:54 +0100

    Change font.

--- dwm-6.0/config.h
+++ dwm-6.0/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "ohsnap:pixelsize=10";
+static const char font[]            = "GohuFont:pixelsize=11";
 static const unsigned int systrayspacing = 2; /* systray spacing */
 static const Bool showsystray = True; 
 
