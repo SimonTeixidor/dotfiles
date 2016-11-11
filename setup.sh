7a74944a3224c6d013c2c8f88da76886c04db7f6
Simon Persson <simon@flaskpost.org>
2016-11-11 23:19:10 +0100

    Clean up home dir.

--- setup.sh
+++ setup.sh
@@ -8,15 +8,19 @@ if [ "$DEST" == "" ]; then
 fi
 
 cp mpd/mpdconf $DEST/.mpdconf
-cp urxvt/Xdefaults $DEST/.Xdefaults
+
+if [ ! -d "$DEST/.config/X11/" ]; then
+	mkdir "$DEST/.config/X11/"
+fi
+cp urxvt/Xdefaults $DEST/.config/X11/Xresources
 
 cp -r ncmpcpp $DEST/.ncmpcpp
 
-if [ ! -d "$DEST/.vim/colors" ]; then
-	mkdir -p "$DEST/.vim/colors"
+if [ ! -d "$DEST/.config/vim/colors" ]; then
+	mkdir -p "$DEST/.config/vim/colors"
 fi
 
-wget https://raw.githubusercontent.com/xero/sourcerer/master/sourcerer.vim -O $DEST/.vim/colors/sourcerer.vim
+wget https://raw.githubusercontent.com/xero/sourcerer/master/sourcerer.vim -O $DEST/.config/vim/colors/sourcerer.vim
 
 cd dwm-6.0
 make
@@ -30,13 +34,14 @@ cp misc/xinitrc $DEST/.xinitrc
 
 cp scripts/startdwm $DEST/bin/
 
-cp vim/vimrc $DEST/.vimrc
+cp vim/vimrc $DEST/.config/vim/
+cp vim/xdg.vim $DEST/.config/vim/
 
-cp bash/dircolors $DEST/.dircolors
+cp bash/dircolors $DEST/.config/dircolors/config
 
-cp screen/screenrc $DEST/.screenrc
+cp screen/screenrc $DEST/.config/screen/screenrc
 
-cp bash/profile $DEST/.profile
+cp bash/profile $DEST/.bash_profile
 
 if [ ! -d "$DEST/.vimperator" ]; then
 	mkdir -p "$DEST/.vimperator/"
@@ -46,22 +51,27 @@ cp -r vimperator/ohsnap.vimp $DEST/.vimperator/
 cp vimperator/vimperatorrc $DEST/.vimperatorrc
 
 
-if [ ! -d "$DEST/.wall.jpg" ]; then
-	wget http://i.imgur.com/SaDyvKE.jpg0 -O $DEST/.wall.jpg
+if [ ! -d "$DEST/.local/share/wall.jpg" ]; then
+	wget http://i.imgur.com/SaDyvKE.jpg0 -O $DEST/.local/share/wall.jpg
 fi
 
-if [ ! -d "$DEST/.fonts" ]; then
-	mkdir "$DEST/.fonts"
+if [ ! -d "$DEST/.local/share/fonts" ]; then
+	mkdir "$DEST/.local/share/fonts"
 fi
 
-wget https://github.com/koemaeda/gohufont-ttf/raw/master/gohufont-11.ttf -O "$DEST/.fonts/gohufont-11.ttf"
+wget https://github.com/koemaeda/gohufont-ttf/raw/master/gohufont-11.ttf -O "$DEST/.local/share/fonts/gohufont-11.ttf"
 
 cp scripts/stats "$DEST/bin/"
 
 mkfontdir $DEST/.fonts
 mkfontscale $DEST/.fonts
 
-cp xmodmap/Xmodmap $DEST/.Xmodmap
-cp xbindkeys/xbindkeysrc $DEST/.xbindkeysrc
+cp xmodmap/Xmodmap $DEST/.config/X11/Xmodmap
+cp xbindkeys/xbindkeysrc $DEST/.config/X11/xbindkeysrc
 
-cp misc/inputrc $DEST/.inputrc
+if [ ! -d "$DEST/.config/readline" ]; then
+	mkdir "$DEST/.config/readline"
+fi
+cp misc/inputrc $DEST/.config/readline/
+
+cp misc/theanorc $DEST/.config/
