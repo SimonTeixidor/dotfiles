1d05ab834169057cc5bb32553efb7c0893dedb70
Simon Persson <simon@flaskpost.me>
2018-02-26 23:21:27 +0100

    Add mpd controls to dwm.

--- void-pkgs/dwm/files/config.h
+++ void-pkgs/dwm/files/config.h
@@ -1,3 +1,4 @@
+#include "mpdcontrol.c"
 /* See LICENSE file for copyright and license details. */
 
 static const unsigned int systrayspacing = 2; /* systray spacing */
@@ -79,6 +80,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_F1,     mpdchange,      {.i = -1} },
+	{ MODKEY,                       XK_F2,     mpdchange,      {.i = +1} },
+	{ MODKEY,                       XK_Escape, mpdcontrol,     {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
