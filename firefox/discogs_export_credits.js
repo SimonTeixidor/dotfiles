a6fce743c325570dbe8a4519b098457f12d471dc
Simon Persson <simon@flaskpost.org>
2020-04-16 08:36:38 +0200

    Update discogs export script

--- firefox/discogs_export_credits.js
+++ firefox/discogs_export_credits.js
@@ -27,19 +27,36 @@ function copy_tags() {
                 "trombone", "timbales", "tambourine", "synth", "vibraphone", "reeds",
                 "piano", "oud", "organ", "oboe", "percussion", "keyboard",
                 "horn", "harmonica", "flute", "drums", "congas", "clarinet",
-                "cello", "brass", "bass", "accordion"
+                "cello", "brass", "bass", "accordion", "choir", "viola", "chorus master",
+		"dulcian", "harp", "lute", "sackbut", "violone", "theorbo", "viol"
         ];
 
         const ignored_roles = [
-                "design", "producer"
+                "design", "producer", "compose", "liner", "illustration", "cover"
         ];
 
         const engineer_roles = [
-                "engineer"
+                "engineer", "recorder", "mastered", "recorded"
         ];
 
         const mixer_roles = [
                 "mixed", "mixing", "mixer"
+        ];
+  
+        const conductor_roles = [
+                "conductor", "conducted"
+        ];
+  
+        const ensemble_roles = [
+                "ensemble", "orchestra"
+        ];
+  			
+	const lyricist_roles = [
+          "lyricist", "text by"
+        ];
+ 
+	const arranger_roles = [
+          "arranged"
         ];
 
         const any_of = list => elem => list.some(m => elem.toLowerCase().includes(m.toLowerCase()));
@@ -47,6 +64,11 @@ function copy_tags() {
         const is_ignored = any_of(ignored_roles);
         const is_engineer = any_of(engineer_roles);
         const is_mixer = any_of(mixer_roles);
+        const is_conductor = any_of(conductor_roles);
+        const is_ensemble = any_of(ensemble_roles);
+	const is_lyricist = any_of(lyricist_roles);
+	const is_arranger = any_of(arranger_roles);
+
 
         const create_tag = (role, name) => {
                 if(is_performer(role)) {
@@ -55,16 +77,24 @@ function copy_tags() {
                         return `ENGINEER=${name}`;
                 } else if (is_mixer(role)) {
                         return `MIXER=${name}`;
+                } else if (is_conductor(role)) {
+                        return `CONDUCTOR=${name}`;
+                } else if (is_ensemble(role)) {
+                        return `ENSEMBLE=${name}`;
+                } else if (is_lyricist(role)) {
+                        return `LYRICIST=${name}`;
+                } else if (is_arranger(role)) {
+                        return `ARRANGER=${name}`;
                 } else if (!is_ignored(role)) {
                         window.alert(`Unhandled role ${role}`);
                 }
         }
 
         const tags = credits.map(a => create_tag(a[0], a[1])).filter(t => t !== undefined);
-	const tag_string = tags.join("\n");
+        const tag_string = tags.join("\n");
 
-	const output_element = document.createElement("span");
-	output_element.innerText = tag_string;
-	document.querySelector("#credits").appendChild(output_element);
+        const output_element = document.createElement("span");
+        output_element.innerText = tag_string;
+        document.querySelector("#credits").appendChild(output_element);
 }
 
