3105823b5107c08491919fd8d66bb313c9599010
Simon Persson <simon@flaskpost.me>
2023-02-11 17:38:23 +0100

    Updates to bash environment

--- config/bash/bashrc.d/00-xdg-dirs.sh
+++ config/bash/bashrc.d/00-xdg-dirs.sh
@@ -7,8 +7,8 @@ export XDG_CACHE_HOME="$HOME/.cache"
 
 # Fix applications that are not complying
 export GNUPGHOME="$XDG_DATA_HOME"/gnupg
-export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
 export HISTFILE="$XDG_DATA_HOME"/bash/history
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME"/pass 
